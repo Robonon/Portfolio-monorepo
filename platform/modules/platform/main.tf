5e4b7a8160b79b19abde3eccc359d4030d06d01f
Robonon <robin.s.ahlqvist@gmail.com>
2025-12-05 22:07:42 +0100

    [Plat][Update]: platform module to create gitea org, repo and team

--- platform/modules/platform/main.tf
+++ platform/modules/platform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "go-gitea/gitea"
       version = "0.7.0"
     }
+    argocd = {
+      source = "argoproj-labs/argocd"
+      version = "7.12.0"
+    }
   }
 }
 
@@ -32,4 +36,148 @@ resource "gitea_team" "platform_team" {
   organisation = data.gitea_org.company_data.name
   description  = "Platform Team"
   permission   = "owner"
+}
+
+# Kustomize application
+resource "argocd_application" "kustomize" {
+  metadata {
+    name      = "kustomize-app"
+    namespace = "argocd"
+    labels = {
+      test = "true"
+    }
+  }
+
+  cascade = false # disable cascading deletion
+  wait    = true
+
+  spec {
+    project = "myproject"
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "foo"
+    }
+
+    source {
+      repo_url        = "https://github.com/kubernetes-sigs/kustomize"
+      path            = "examples/helloWorld"
+      target_revision = "master"
+      kustomize {
+        name_prefix = "foo-"
+        name_suffix = "-bar"
+        images      = ["hashicorp/terraform:light"]
+        common_labels = {
+          "this.is.a.common" = "la-bel"
+          "another.io/one"   = "true"
+        }
+      }
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = true
+      }
+      # Only available from ArgoCD 1.5.0 onwards
+      sync_options = ["Validate=false"]
+      retry {
+        limit = "5"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+
+    ignore_difference {
+      group         = "apps"
+      kind          = "Deployment"
+      json_pointers = ["/spec/replicas"]
+    }
+
+    ignore_difference {
+      group = "apps"
+      kind  = "StatefulSet"
+      name  = "someStatefulSet"
+      json_pointers = [
+        "/spec/replicas",
+        "/spec/template/spec/metadata/labels/bar",
+      ]
+      # Only available from ArgoCD 2.1.0 onwards
+      jq_path_expressions = [
+        ".spec.replicas",
+        ".spec.template.spec.metadata.labels.bar",
+      ]
+    }
+  }
+}
+
+# Helm application
+resource "argocd_application" "helm" {
+  metadata {
+    name      = "helm-app"
+    namespace = "argocd"
+    labels = {
+      test = "true"
+    }
+  }
+
+  spec {
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default"
+    }
+
+    source {
+      repo_url        = "https://some.chart.repo.io"
+      chart           = "mychart"
+      target_revision = "1.2.3"
+      helm {
+        release_name = "testing"
+        parameter {
+          name  = "image.tag"
+          value = "1.2.3"
+        }
+        parameter {
+          name  = "someotherparameter"
+          value = "true"
+        }
+        value_files = ["values-test.yml"]
+        values = yamlencode({
+          someparameter = {
+            enabled   = true
+            someArray = ["foo", "bar"]
+          }
+        })
+      }
+    }
+  }
+}
+
+resource "argocd_application" "platform_apps" {
+  metadata {
+    name      = "platform-apps"
+    namespace = "argocd"
+    labels = {
+      platform = "platform"
+    }
+  }
+
+  spec {
+    project = "platform-project"
+
+    source {
+        repo_url = "https://github.com/Robonon/Portfolio-monorepo.git"
+        chart = "mychart"
+        path = "/apps/platform-apps"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default"
+    }
+  }
 }
